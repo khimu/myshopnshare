e845257b87ab084a28f715f4a7eca9d373d79ebe
khimung <kim2kim@gmail.com>
2016-07-07 14:59:41 -0700

    Add deploy script

---
+++ deployToServer.sh
@@ -0,0 +1,12 @@
+mvn clean install -DskipTests=true
+
+
+ssh root@107.170.234.144 'service tomcat7 stop'
+
+ssh root@107.170.234.144 'rm -rf /var/lib/tomcat7/webapps/ROOT'
+
+scp mss-war/target/myshopnshare.war root@107.170.234.144:/var/lib/tomcat7/webapps/ROOT.war
+
+ssh root@107.170.234.144 'service tomcat7 start'
+
+ssh root@107.170.234.144 'ps aux | grep "tomcat"'
